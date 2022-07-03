dc1a3235ad4bd9c77d0ae6b7f22b201520b21491
Mishu Rahman <mishu.cse616@gmail.com>
2022-07-04 01:25:27 +0600

    basic model and migration config

--- site/app/Models/visitor.php
+++ site/app/Models/visitor.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class visitor extends Model
 {
-    use HasFactory;
+   public $table='visitors';
+   public $primaryKey='id';
+   public $incrementing='true';
+   public $keyType='int';
+   public $timestamps='false';
 }
