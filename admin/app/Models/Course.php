22fee2f2419e9100a760763d5e830c4df6defc46
Mishu Rahman <mishu.cse616@gmail.com>
2022-07-19 16:07:05 +0600

    Update Course.php

--- admin/app/Models/Course.php
+++ admin/app/Models/Course.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    public $table='courses';
+    public $primaryKey='id';
+    public $keyType='int';
+    public $autoIncrementing='true';
+    public $timestamps='true';
 }
